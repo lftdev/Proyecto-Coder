f07624bc83aa038c6e053ce918c1abf30e22956b
hydrae-soft <torres.lf21@gmail.com>
2023-05-03 14:14:37 -0300

    Improvements ver5

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
-const btn_menu = document.querySelector("#btn-menu")
-btn_menu.addEventListener("click", on_click_menu)
+const header = document.querySelector('header')
+const nav_menu_header = document.querySelector('#nav-menu-header')
+const header_social_nav = document.querySelector('header .social-nav')
+const btn_menu_span = document.querySelectorAll('#btn-menu span')
+document.querySelector('#btn-menu').addEventListener('click', on_click_menu)
 
-const signup_window = document.querySelector("#div-signup-window")
-
-window.addEventListener("scroll", () => {
+const mutable_header = document.querySelector('header.mutable')
+window.addEventListener('scroll', () => {
     sw_color_on_scroll(window.scrollY)
 });
 
 function on_click_menu() {
-    document.querySelector("header").classList.toggle("deployed")
-    document.querySelector("#nav-menu-header").classList.toggle("hide-on-screen-sm")
-    document.querySelector("header .social-nav").classList.toggle("set-closed")
-    document.querySelectorAll("#btn-menu span").forEach(span => {
-        span.classList.toggle("deployed")
+    header.classList.toggle('deployed')
+    nav_menu_header.classList.toggle('set-closed')
+    header_social_nav.classList.toggle('set-closed')
+    btn_menu_span.forEach(span => {
+        span.classList.toggle('deployed')
     })
-    
 }
 
 function sw_color_on_scroll(scrollY) {
-    let mutable_header = document.querySelector("header.mutable")
     if (mutable_header != null)
-        scrollY >= window.innerHeight? mutable_header.classList.add("sw-color-on-scroll") : mutable_header.classList.remove("sw-color-on-scroll")
+        scrollY >= window.innerHeight? mutable_header.classList.add('sw-color-on-scroll') : mutable_header.classList.remove('sw-color-on-scroll')
 }
