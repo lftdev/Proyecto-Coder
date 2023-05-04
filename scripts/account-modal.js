13e2cbeee342aa65be10b858b807ac6eefdf7e07
hydrae-soft <torres.lf21@gmail.com>
2023-05-04 12:53:29 -0300

    SASS: added mixins set-size, set-colors.

--- scripts/account-modal.js
+++ scripts/account-modal.js
@@ -1,8 +1,10 @@
-document.querySelector(".btn-close-modal").addEventListener("click", on_click_btn_signup)
+const html = document.querySelector('html')
+const main_home_modal = document.querySelector('#main-home .div-modal')
 
-document.querySelector("#btn-signup").addEventListener("click", on_click_btn_signup)
+document.querySelector('.btn-close-modal').addEventListener('click', on_click_btn_signup)
+document.querySelector('#btn-signup').addEventListener('click', on_click_btn_signup)
 
 function on_click_btn_signup() {
-    document.querySelector("#main-home .div-modal").classList.toggle("set-closed")
-    document.querySelector("html").classList.toggle("on-modal-show")
+    main_home_modal.classList.toggle('set-closed')
+    html.classList.toggle('on-modal-show')
 }
